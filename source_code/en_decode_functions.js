3bbb6434aa0a793d38e1ce09ceb901bd709f56f9
Ray Rubens <148541376+peppermintbird@users.noreply.github.com>
2024-03-15 15:59:48 -0300

    challenge-decoder

---
+++ source_code/en_decode_functions.js
@@ -0,0 +1,35 @@
+function encode() {
+  const input = document.getElementById('input').value;
+  const encoded = caesarCipher(input, 3); //shift value
+  displayResult(encoded);
+}
+
+function decode() {
+  const input = document.getElementById('input').value;
+  const decoded = caesarCipher(input, -3); //opposite shift value for decoding
+  displayResult(decoded);
+}
+
+function caesarCipher(str, shift) {
+  return str.replace(/[a-zA-Z]/g, function(char) {
+    const isUpperCase = char === char.toUpperCase();
+    const offset = isUpperCase ? 65 : 97;
+    return String.fromCharCode((char.charCodeAt(0) - offset + shift + 26) % 26 + offset);
+  });
+}
+
+function displayResult(result) {
+  const output = document.getElementById('output');
+  output.value = result;
+  output.classList.add('#output');
+}
+
+function copy() {
+  const output = document.getElementById('output');
+  output.select();
+  document.execCommand('copy');
+}
+
+
+
+ 
